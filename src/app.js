5da73f812bb2eac0d50ba6d4227e4468259fb8e6
YactayoC <syactayocrisostomo@gmail.com>
2024-11-16 11:40:09 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const routes = require("./routes");
 
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api", routes);
 
